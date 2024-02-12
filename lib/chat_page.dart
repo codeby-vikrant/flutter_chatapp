a12bdae2f99c5113d827ae26424b8e0e849e24d5
Vikrant Vani <workwithvikrant0111@gmail.com>
2024-02-12 22:49:10 +0530

    Updated

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -5,14 +5,37 @@ import 'package:flutter_study/widgets/chat_bubble.dart';
 import 'models/chat_message_entity.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({super.key});
+   ChatPage({super.key});
+
+  List<ChatMessageEntity> _messages = [
+    ChatMessageEntity(
+        author: Author(username: "vicky10"),
+        createdAt: 2131231242,
+        id: '1',
+        text: 'First text'),
+    ChatMessageEntity(
+        author: Author(username: "vicky10"),
+        createdAt: 2131231242,
+        id: '1',
+        text: 'Second text',
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOkZdPituG-u78CL9TsJ7kP-oluyq1-7-oRLU46HCjrw&s'),
+    ChatMessageEntity(
+      author: Author(username: "Vedant"),
+      createdAt: 2131231242,
+      id: '1',
+      text: 'Third text',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     final username = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hi! $username', textAlign: TextAlign.center,),
+        title: Text(
+          'Hi! $username',
+          textAlign: TextAlign.center,
+        ),
         backgroundColor: Colors.transparent,
         elevation: 1,
         actions: [
@@ -28,18 +51,13 @@ class ChatPage extends StatelessWidget {
         children: [
           Expanded(
             child: ListView.builder(
-                itemCount: 10,
+                itemCount: _messages.length,
                 itemBuilder: (context, index) {
                   return ChatBubble(
-                      alignment: index % 2 == 0
-                          ? Alignment.centerLeft
-                          : Alignment.centerRight,
-                      entity: ChatMessageEntity(
-                      id: "1234",
-                      text: "Hello This is Vikrant!",
-                      createdAt: DateTime.now().microsecondsSinceEpoch,
-                      author: Author(username: 'vicky01'),
-                  ));
+                      alignment: _messages[index].author.username == 'vicky10'
+                          ? Alignment.centerRight
+                          : Alignment.centerLeft,
+                      entity: _messages[index]);
                 }),
           ),
           ChatInput(),
